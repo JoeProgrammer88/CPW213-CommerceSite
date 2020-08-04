1573b0d88f7ba69af04c4eff3f0e53d1b41ea04d
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-08-03 17:51:01 -0700

    Delete product implemented

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -90,6 +90,7 @@ namespace eCommerceSite.Controllers
             return View(p);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
             Product p = await (from prod in _context.Products
@@ -97,6 +98,23 @@ namespace eCommerceSite.Controllers
                         select prod).SingleAsync();
 
             return View(p);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Product p = await (from prod in _context.Products
+                        where prod.ProductId == id
+                        select prod).SingleAsync();
+
+            _context.Entry(p).State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+
+            TempData["Message"] = $"{p.Title} was deleted";
+
+            return RedirectToAction("Index");
         }
     }
 }
